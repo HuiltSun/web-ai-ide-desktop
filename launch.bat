99163da4aedc54ab98ab365fd94ea46186e1c201
HuiltSun <HuiltSun@users.noreply.github.com>
2026-04-09 10:52:52 +0800

    chore: 更新启动脚本中的可执行文件路径

--- launch.bat
+++ launch.bat
@@ -2,4 +2,4 @@
 chcp 65001 > nul
 echo Starting Web AI IDE...
 echo.
-"E:\web\web-ai-ide\release\release-2026-04-08T12-44-29\Web AI IDE Setup 1.0.0.exe"
+"E:\web\web-ai-ide\release\release-2026-04-09T02-51-57\Web AI IDE Setup 1.0.0.exe"
